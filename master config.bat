35a687c6c71fad57690414fdc3d59f569f7bdf90
Coach.Sludge <141787395+CoachSludge@users.noreply.github.com>
2023-11-02 01:03:08 -0600

    Update master config.bat

--- master config.bat
+++ master config.bat
@@ -38,58 +38,60 @@ set "newClientSecret="
 
 
 
+
+
 :: Step 1: Edit config.js
 (
-  echo const platformDefaults = {^
-    win32: "C:/Program Files/VideoLAN/VLC/vlc.exe",^
-    winalt: "C:/Program Files (x86)/VideoLAN/VLC/vlc.exe",^
-    linux: "/usr/bin/vlc",^
-    unix: "/usr/bin/vlc",^
-    darwin: "/Applications/VLC.app/Contents/MacOS/VLC",^
-  };^
-  echo.^
-  echo const vlcPath = "%newVlcPath%";^
-  echo.^
-  echo const richPresenseSettings = {^
-    id: "%newId%",^
-    updateInterval: 500,^
-    removeAfter: 5000,^
-  };^
-  echo.^
-  echo const iconNames = {^
-    pause: "https://i.imgur.com/CCg9fxf.png",^
-    playing: "https://i.imgur.com/8IYhOc2.png",^
-    vlc: "https://i.imgur.com/7CRaCeT.png",^
-  };^
-  echo.^
-  echo const spotify = {^
-    clientID: "%newClientID%",^
-    clientSecret: "%newClientSecret%",^
-  };^
-  echo.^
-  echo const vlcConfig = {^
-    password: "%newPassword%",^
-    port: 8080,^
-    address: "localhost",^
-  };^
-  echo.^
-  echo const logUpdates = false;^
-  echo.^
+  echo const platformDefaults = { 
+  echo   win32: "C:/Program Files/VideoLAN/VLC/vlc.exe", 
+  echo   winalt: "C:/Program Files (x86)/VideoLAN/VLC/vlc.exe", 
+  echo   linux: "/usr/bin/vlc", 
+  echo   unix: "/usr/bin/vlc", 
+  echo   darwin: "/Applications/VLC.app/Contents/MacOS/VLC", 
+  echo }; 
+  echo. 
+  echo const vlcPath = "%newVlcPath%"; 
+  echo. 
+  echo const richPresenseSettings = { 
+  echo   id: "%newId%", 
+  echo   updateInterval: 500, 
+  echo   removeAfter: 5000, 
+  echo }; 
+  echo. 
+  echo const iconNames = { 
+  echo   pause: "https://i.imgur.com/CCg9fxf.png", 
+  echo   playing: "https://i.imgur.com/8IYhOc2.png", 
+  echo   vlc: "https://i.imgur.com/7CRaCeT.png", 
+  echo }; 
+  echo. 
+  echo const spotify = { 
+  echo   clientID: "%newClientID%", 
+  echo   clientSecret: "%newClientSecret%", 
+  echo }; 
+  echo. 
+  echo const vlcConfig = { 
+  echo   password: "%newPassword%", 
+  echo   port: 8080, 
+  echo   address: "localhost", 
+  echo }; 
+  echo. 
+  echo const logUpdates = false; 
+  echo. 
   echo module.exports = { platformDefaults, vlcPath, richPresenseSettings, vlcConfig, spotify, iconNames, logUpdates };
-) > config.js
+) > "%newPath%\Storage\config.js"
 
 :: Step 2: Update path in setup.bat
 (
-  echo @echo off^
-  echo cd /d "%newPath%"^
+  echo @echo off
+  echo cd /d "%newPath%"
   echo npm install
-) > setup.bat
+) > "%newPath%\setup.bat"
 
 :: Step 3: Update path in hide_node.vbs
 (
-  echo Set objShell = WScript.CreateObject("WScript.Shell")^
+  echo Set objShell = WScript.CreateObject("WScript.Shell")
   echo objShell.Run "cmd /k node %newPath%", 0, False
-) > hide_node.vbs
+) > "%newPath%\hide_node.vbs"
 
 :: Step 4: Run updated setup.bat
 call "%newPath%\setup.bat"
